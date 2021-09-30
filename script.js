914fc5be216d9aca513a8d155447cdb8e733a60c
tamilselvanks007 <87566691+tamilselvanks007@users.noreply.github.com>
2021-09-30 20:31:25 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,9 +6,29 @@ function foo(){
     let result=await data.json();
     console.log(result);
     for(var i in result){
-        var compute=result[i].Confirmed
-       var div=document.createElement('div');
+        var compute=result[i].Active; 
+        var div=document.createElement('div');
+       div.innerHTML=`Active:${compute}`;
+       document.body.append(div);
+
+        var compute=result[i].Confirmed; 
+        var div=document.createElement('div');
        div.innerHTML=`Confirmed cases:${compute}`;
+       document.body.append(div);
+      
+       var compute=result[i].Date;
+       var div=document.createElement('div');
+       div.innerHTML=`Date:${compute}`;
+       document.body.append(div);
+      
+       var compute=result[i].Deaths;
+       var div=document.createElement('div');
+       div.innerHTML=`Deaths:${compute}`;
+       document.body.append(div);
+
+       var compute=result[i].Recovered;
+       var div=document.createElement('div');
+       div.innerHTML=`Recovered:${compute}`;
        document.body.append(div);
     }
      
